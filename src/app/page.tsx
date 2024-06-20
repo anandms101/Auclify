af2ed98e7328215be02945e131a3ace224e8b57d
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-06-20 22:46:09 +0530

    shadcn components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import {database} from "@/db/database";
 import {bids as bidsSchema} from "@/db/schema";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { revalidatePath } from "next/cache";
+
+
 
 export default async function Home() {
 
@@ -10,9 +15,10 @@ export default async function Home() {
     <main className="container mx-auto py-12">
       <form action={async (formData: FormData) => { "use server" 
       await database.insert(bidsSchema).values({});
+      revalidatePath("/");
       }}>
-        <input name="bid" placeholder="Bid" />
-        <button type="submit">Submit</button>
+        <Input name="bid" placeholder="Bid" />
+        <Button type="submit">Submit</Button>
       </form>
 
       {
