3f0ae91bcbf65628d183f96d0f4d265ea01b5ce5
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-06-20 18:53:40 +0530

    shacn added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
 import Image from "next/image";
 import {database} from "@/db/database";
-import {bids} from "@/db/schema";
+import {bids as bidsSchema} from "@/db/schema";
 
-export default function Home() {
+export default async function Home() {
+
+  const bids = await database.query.bids.findMany({});
+
   return (
-    <main className="">
+    <main className="container mx-auto py-12">
       <form action={async (formData: FormData) => { "use server" 
-      await database.insert(bids).values({});
+      await database.insert(bidsSchema).values({});
       }}>
         <input name="bid" placeholder="Bid" />
         <button type="submit">Submit</button>
       </form>
+
+      {
+        bids.map((bid) => (
+          <div key={bid.id}>
+            <h2>{bid.id}</h2>
+          </div>
+        ))
+      }
     </main>
   );
 }
