91e6ae4d5997117abcffc2281e4efa2fa0d4d1e2
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-06-21 00:02:49 +0530

    NextAuth configured and sign in and out working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,33 @@
 import Image from "next/image";
-import {database} from "@/db/database";
-import {bids as bidsSchema} from "@/db/schema";
+import { database } from "@/db/database";
+import { bids as bidsSchema } from "@/db/schema";
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { revalidatePath } from "next/cache";
+import { SignIn } from "@/components/sign-in";
+import { auth } from "@/auth";
+import { SignOut } from "@/components/sign-out";
 
 
 
 export default async function Home() {
 
   const bids = await database.query.bids.findMany({});
+  const session = await auth()
 
   return (
     <main className="container mx-auto py-12">
-      <form action={async (formData: FormData) => { "use server" 
-      await database.insert(bidsSchema).values({});
-      revalidatePath("/");
+
+      {session ?
+        <Button variant="destructive"><SignOut /></Button>
+        :
+        <Button variant="default"><SignIn /></Button>
+      }
+
+      <form action={async (formData: FormData) => {
+        "use server"
+        await database.insert(bidsSchema).values({});
+        revalidatePath("/");
       }}>
         <Input name="bid" placeholder="Bid" />
         <Button type="submit">Submit</Button>
