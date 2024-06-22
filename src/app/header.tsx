5c433e79f11319863b91a6e9135b22556c5c0bb8
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-06-22 15:05:44 +0530

    header replaced with shadcn component

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,39 +1,63 @@
-import Image from "next/image"
+import Image from "next/image";
 import { SignOut } from "@/components/sign-out";
 import { auth } from "@/auth";
 import { SignIn } from "@/components/sign-in";
 import Link from "next/link";
-
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "@radix-ui/react-navigation-menu";
 
 export async function Header() {
-
     const session = await auth();
 
     return (
         <>
-            <div className="bg-gray-100 ">
+            {/* <div className="bg-gray-100 ">
                 <div className="container flex justify-between items-center max-h-20">
-                    <div className="flex pt-4">
-                        <Link href="/" >
+                    <div className="flex pt-4 items-center">
+                        <Link href="/">
                             <div className="hover:scale-110 transition-transform duration-300">
-                                <Image src="/images/brand-logo/logo_transparent.png" alt="Auclify" width={130} height={130} />
+                                <Image
+                                    src="/images/brand-logo/logo_transparent.png"
+                                    alt="Auclify"
+                                    width={130}
+                                    height={130}
+                                />
                             </div>
                         </Link>
+                        <div>
+                            <Link href="/items/create/">
+                                Create Item
+                            </Link>
+                        </div>
                     </div>
                     <div className="flex items-center">
-                        <span className="pr-2">
-                            {session?.user?.name}
-                        </span>
-                        <span>
-                            {session ?
-                                <SignOut />
-                                :
-                                <SignIn />
-                            }
-                        </span>
+                        <span className="pr-2">{session?.user?.name}</span>
+                        <span>{session ? <SignOut /> : <SignIn />}</span>
                     </div>
                 </div>
-            </div>
+            </div> */}
+            <NavigationMenu className="bg-gray-100">
+                <NavigationMenuList className="flex flex-row items-center justify-between max-h-20">
+                    <div className="flex flex-row items-center">
+                        <NavigationMenuItem className="pt-4 hover:scale-110 transition-transform duration-300">
+                            <Image
+                                src="/images/brand-logo/logo_transparent.png"
+                                alt="Auclify"
+                                width={130}
+                                height={130}
+                            />
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <Link href="/item/create/">
+                                Create an Item
+                            </Link>
+                        </NavigationMenuItem>
+                    </div>
+                    <div className="flex flex-row mr-4 items-center">
+                        <span className="pr-2">{session?.user?.name}</span>
+                        <span>{session ? <SignOut /> : <SignIn />}</span>
+                    </div>
+                </NavigationMenuList>
+            </NavigationMenu>
         </>
-    )
+    );
 }
