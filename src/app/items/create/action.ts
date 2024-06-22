ab607123ef1bc9f2781d99d0372a63755062c54c
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-06-22 18:51:59 +0530

    image getting stored in the s3

---
+++ src/app/items/create/action.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import { database } from "@/db/database";
+import { items } from "@/db/schema";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { getSignedUrlForS3Object } from "@/lib/s3";
+
+export async function createUploadUrl(key: string, type: string) {
+  return await getSignedUrlForS3Object(key, type);
+}
+
+export async function createItemAction(formData: FormData){
+    const session = await auth()
+    
+    if (!session || !session.user) {
+        return null
+    }
+    
+    const file = formData.get("image") as File;
+    const uploadUrl = await createUploadUrl(file.name, file.type);
+    const uploadFormData = new FormData();
+    await fetch(uploadUrl, {
+        method: "PUT",
+        body: uploadFormData,
+        headers: {
+        "Content-Type": file.type,
+        }
+    });
+    await database.insert(items).values({
+        name: formData.get("name") as string,
+        userId: session?.user?.id!,
+        startingPrice: Number(formData.get("startingPrice")),
+    });
+    redirect("/")
+}
--- src/app/items/create/page.tsx
+++ src/app/items/create/page.tsx
@@ -1,30 +1,19 @@
-import { database } from "@/db/database";
-import { items } from "@/db/schema";
+'use client';
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
+import { createItemAction } from "./action";
 
 export default async function Home() {
-    
-  const session = await auth()
-
-  if (!session || !session.user) {
-    return null
-  }
-
   return (
     <main className="container mx-auto py-12">
       <h1 className="text-4xl font-bold">Items</h1>
-      <form className="flex flex-col border p-8 rounded-xl space-y-4 max-w-md" action={async (formData: FormData) => {
-        "use server"
-        await database.insert(items).values({
-          name: formData.get("name") as string,
-          userId: session?.user?.id!,
-          startingPrice: Number(formData.get("startingPrice")),
-        });
-        redirect("/");
-      }}>
+      <form className="flex flex-col border p-8 rounded-xl space-y-4 max-w-md"
+        onSubmit={async (event) => {
+          event.preventDefault()
+          const form = event.currentTarget as HTMLFormElement;
+          const formData = new FormData(form);
+          await createItemAction(formData);
+        }}>
         <Input required className="max-w-lg" name="name" placeholder="Name your item" />
         <Input required type="number" name="startingPrice" placeholder="Starting price" />
         <Input required type="file" name="image" />
